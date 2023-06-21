258a221c834a2e76c4b4514be5142358c8eb5609
James Carlson <jxxcarlson@gmail.com>
2023-06-21 08:31:15 -0400

    Preparing for v2

---
+++ src/Evergreen/V2/Credentials.elm
@@ -0,0 +1,5 @@
+module Evergreen.V2.Credentials exposing (..)
+
+
+type Credentials
+    = V1 String String
---
+++ src/Evergreen/V2/User.elm
@@ -0,0 +1,14 @@
+module Evergreen.V2.User exposing (..)
+
+import Time
+
+
+type alias User =
+    { username : String
+    , id : String
+    , realname : String
+    , email : String
+    , created : Time.Posix
+    , modified : Time.Posix
+    , locked : Bool
+    }
