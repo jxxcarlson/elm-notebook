f6f966ccd544ee46c0f28d87d21ea57215a46027
James Carlson <jxxcarlson@gmail.com>
2023-06-21 23:11:15 -0400

    Preparing for v6

---
+++ src/Evergreen/V6/User.elm
@@ -0,0 +1,14 @@
+module Evergreen.V6.User exposing (..)
+
+import Time
+
+
+type alias User =
+    { username : String
+    , id : String
+    , realname : String
+    , email : String
+    , created : Time.Posix
+    , modified : Time.Posix
+    , locked : Bool
+    }
