b12af22a031297bcc4363427e92a5d8840fb7357
James Carlson <jxxcarlson@gmail.com>
2023-06-22 16:51:23 -0400

    Simplify landing page text

--- src/View/BodyNotSignedIn.elm
+++ src/View/BodyNotSignedIn.elm
@@ -12,7 +12,7 @@ view : FrontendModel -> Element msg
 view model =
     E.column [ E.height (E.px (View.Geometry.mainColumnHeight model)), E.centerX, E.spacing 18 ]
         [ E.el [ Font.color (E.rgb 0.8 0.8 0.8), Font.size 32, E.centerX, E.paddingEach { left = 0, right = 0, top = 48, bottom = 24 } ] (E.text "Elm Livebook")
-        , E.image [ E.height (E.px (View.Geometry.mainColumnHeight model - 410)), E.centerX, E.centerY ]
+        , E.image [ E.height (E.px (View.Geometry.mainColumnHeight model - 360)), E.centerX, E.centerY ]
             { src = "https://imagedelivery.net/9U-0Y4sEzXlO6BXzTnQnYQ/30f08d58-dbce-42a0-97a6-512735707700/public"
             , description = "bird"
             }
@@ -30,22 +30,19 @@ view model =
             , E.width (E.px (View.Geometry.appWidth model - 370))
             ]
             [ E.paragraph [ E.spacing 8 ]
-                [ E.text "Welcome to Elm Livebook — an app in divided into cells in which you can run Elm code.  There is much work to be done — "
-                , E.text "and hopefully much more can be done before Elm Camp starts. "
-                , E.text "@minibill's elm-interpreter is the key to the whole thing, and it has now been wired in, so we have a proof of concept. "
+                [ E.text "Welcome to Elm Livebook — an app like Jupyter notebooks or Elixir's Livebook. "
+                , E.text "Documents are made up of cells which contain both text and Elm code. "
                 ]
             , E.row [ E.height (E.px 8) ] []
             , E.paragraph [ E.spacing 8 ]
-                [ E.text "At the moment each user has a single notebook with title 'Scratchpad'.  This limitation will be rectified soon. "
-                , E.text "The scratchpad document is saved (if need be) every five seconds. "
+                [ E.text "Click on a cell to edit it."
+                , E.text "To run the Elm code in cell, click the 'eval' button or type Ctrl-Enter. "
+                , E.text "The engine that runs the app @minibill's elm-interpreter. "
                 ]
-            , E.row
-                [ E.height (E.px 8) ]
-                []
+            , E.row [ E.height (E.px 8) ] []
             , E.paragraph [ E.spacing 8 ]
-                [ E.text "There is much to do besides the usual fixing of bugs and tuning the UI, e.g.,  CRUD for notebooks. "
-                , E.text "Especially vexing is an authentication bug that appears randomly on sign up.  It needs to be fixed. "
-                , E.text "If you want to help, please reach out to me on Slack (jxxcarlson). "
+                [ E.text "There is much to do besides the usual fixing of bugs, tuning the UI, and adding features (within reason!). "
+                , E.text "One thing we have in mind is a way of sharing notebooks. "
                 ]
             ]
         ]
