a5d3bfc356a7b53e31687e7ed7714bbf2705a60e
James Carlson <jxxcarlson@gmail.com>
2023-06-23 02:00:16 -0400

    Update landing page text

--- src/View/BodyNotSignedIn.elm
+++ src/View/BodyNotSignedIn.elm
@@ -12,7 +12,7 @@ view : FrontendModel -> Element msg
 view model =
     E.column [ E.height (E.px (View.Geometry.mainColumnHeight model)), E.centerX, E.spacing 18 ]
         [ E.el [ Font.color (E.rgb 0.8 0.8 0.8), Font.size 32, E.centerX, E.paddingEach { left = 0, right = 0, top = 48, bottom = 24 } ] (E.text "Elm Livebook")
-        , E.image [ E.height (E.px (View.Geometry.mainColumnHeight model - 360)), E.centerX, E.centerY ]
+        , E.image [ E.height (E.px (View.Geometry.mainColumnHeight model - 310)), E.centerX, E.centerY ]
             { src = "https://imagedelivery.net/9U-0Y4sEzXlO6BXzTnQnYQ/30f08d58-dbce-42a0-97a6-512735707700/public"
             , description = "bird"
             }
@@ -38,12 +38,6 @@ view model =
                 [ E.text "Click on a cell to edit it."
                 , E.text "Then click the 'eval' button or type Ctrl-Enter to run the code."
                 ]
-                , E.text "The engine that runs the app is @minibill's elm-interpreter. "
-                ]
-            , E.row [ E.height (E.px 8) ] []
-            , E.paragraph [ E.spacing 8 ]
-                [ E.text "There is much to do besides the usual fixing of bugs, tuning the UI, and adding features (within reason!). "
-                , E.text "One thing we have in mind is a way of sharing notebooks. "
-                ]
+            , E.text "The engine that runs the app is @minibill's elm-interpreter. "
             ]
         ]
