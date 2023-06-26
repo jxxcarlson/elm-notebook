0c1dc7d90d39f0ff2dbe4433e59dde71ae5bb405
James Carlson <jxxcarlson@gmail.com>
2023-06-26 11:24:20 +0000

    Add image of cells to landing page; edit text

--- src/View/BodyNotSignedIn.elm
+++ src/View/BodyNotSignedIn.elm
@@ -12,9 +12,10 @@ view : FrontendModel -> Element msg
 view model =
     E.column [ E.height (E.px (View.Geometry.mainColumnHeight model)), E.centerX, E.spacing 18 ]
         [ E.el [ Font.color (E.rgb 0.8 0.8 0.8), Font.size 32, E.centerX, E.paddingEach { left = 0, right = 0, top = 48, bottom = 24 } ] (E.text "Elm Livebook")
-        , E.image [ E.height (E.px (View.Geometry.mainColumnHeight model - 310)), E.centerX, E.centerY ]
-            { src = "https://imagedelivery.net/9U-0Y4sEzXlO6BXzTnQnYQ/30f08d58-dbce-42a0-97a6-512735707700/public"
-            , description = "bird"
+        , E.image [ E.width (E.px 600), E.centerX, E.centerY ]
+            { --src = "https://imagedelivery.net/9U-0Y4sEzXlO6BXzTnQnYQ/30f08d58-dbce-42a0-97a6-512735707700/public"
+              src = "https://imagedelivery.net/9U-0Y4sEzXlO6BXzTnQnYQ/f251339a-3b7e-4131-f42f-21dfd5b77500/public"
+            , description = "Cells in Elm Livebook"
             }
         , E.column
             [ View.Style.fgGray 0.6
@@ -26,8 +27,8 @@ view model =
             , E.scrollbarY
 
             --, E.height (E.px (View.Geometry.mainColumnHeight model))
-            , E.height (E.px 400)
-            , E.width (E.px (min 500 (View.Geometry.appWidth model - 300)))
+            , E.height (E.px 600)
+            , E.width (E.px (min 600 (View.Geometry.appWidth model - 300)))
             ]
             [ E.paragraph [ E.spacing 8 ]
                 [ E.text "Welcome to Elm Livebook — an app like Jupyter notebooks or Elixir's Livebook. "
@@ -37,6 +38,10 @@ view model =
             , E.paragraph [ E.spacing 8 ]
                 [ E.text "Please look at the manual before you start. (Click on 'Manual' button in header.) "
                 , E.text "See also the public notebook 'Welcome to Elm Livebook'. "
+                ]
+            , E.row [ E.height (E.px 8) ] []
+            , E.paragraph [ E.spacing 8 ]
+                [ E.text "Quick start: type ctrl-Enter in a cell to evaluate it "
                 ]
             , E.row [ E.height (E.px 8) ] []
             , E.text "The engine that runs the app is @minibill's elm-interpreter. "
