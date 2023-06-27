2ed1e10dfb8786c43f6845c4f86e7f83a0cae028
James Carlson <jxxcarlson@gmail.com>
2023-06-27 18:33:31 +0200

    move code around

--- src/LiveBook/Process.elm
+++ src/LiveBook/Process.elm
@@ -3,8 +3,29 @@ module LiveBook.Process exposing (cellContents)
 import List.Extra
 
 
+{-| This function takes a list of lines and returns a list of lines with with
+various changes:
+
+    - Code blocks are surrounded by ``` and ``` (markdown code block)
+    - All lines in sequences of lines beginning with #, except for the last, are suffixed with " \\"
+
+-}
 cellContents cell =
     cell.text |> List.Extra.dropWhileRight (\line -> String.trim line == "") |> runMachine
+
+
+runMachine : List String -> List String
+runMachine input =
+    loop
+        { input = input
+        , output = []
+        , internalState = InText
+        , lineCount = 0
+        , linesOfCode = 0
+        , numberOfLines = List.length input
+        }
+        nextStep
+        |> List.reverse
 
 
 type alias State =
@@ -134,20 +155,6 @@ nextStep state =
                                         line :: state.output
                                 , internalState = InCode
                             }
-
-
-runMachine : List String -> List String
-runMachine input =
-    loop
-        { input = input
-        , output = []
-        , internalState = InText
-        , lineCount = 0
-        , linesOfCode = 0
-        , numberOfLines = List.length input
-        }
-        nextStep
-        |> List.reverse
 
 
 type Step state a
