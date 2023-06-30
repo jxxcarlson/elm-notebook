6a1c499e6c9253da3408b3a1d70dbcfd5bebd0e7
James Carlson <jxxcarlson@gmail.com>
2023-06-30 07:44:17 +0200

    Preparing for v82

---
+++ src/Evergreen/V82/Credentials.elm
@@ -0,0 +1,5 @@
+module Evergreen.V82.Credentials exposing (..)
+
+
+type Credentials
+    = V1 String String
---
+++ src/Evergreen/V82/Types.elm
@@ -0,0 +1,244 @@
+module Evergreen.V82.Types exposing (..)
+
+import Browser
+import Browser.Dom
+import Browser.Navigation
+import Dict
+import Evergreen.V82.Authentication
+import Evergreen.V82.User
+import File
+import Keyboard
+import Lamdera
+import Random
+import Time
+import Url
+
+
+type MessageStatus
+    = MSWhite
+    | MSYellow
+    | MSGreen
+    | MSRed
+
+
+type alias Message =
+    { txt : String
+    , status : MessageStatus
+    }
+
+
+type AppState
+    = Loading
+    | Loaded
+
+
+type AppMode
+    = AMWorking
+    | AMEditTitle
+
+
+type VisualType
+    = VTChart
+    | VTImage
+
+
+type CellValue
+    = CVNone
+    | CVString String
+    | CVVisual VisualType (List String)
+
+
+type CellState
+    = CSEdit
+    | CSView
+
+
+type alias Cell =
+    { index : Int
+    , text : List String
+    , value : CellValue
+    , cellState : CellState
+    }
+
+
+type alias Book =
+    { id : String
+    , dirty : Bool
+    , slug : String
+    , origin : Maybe String
+    , author : String
+    , createdAt : Time.Posix
+    , updatedAt : Time.Posix
+    , public : Bool
+    , title : String
+    , cells : List Cell
+    , currentIndex : Int
+    }
+
+
+type DeleteNotebookState
+    = WaitingToDeleteNotebook
+    | CanDeleteNotebook
+
+
+type ShowNotebooks
+    = ShowUserNotebooks
+    | ShowPublicNotebooks
+
+
+type SignupState
+    = ShowSignUpForm
+    | HideSignUpForm
+
+
+type PopupState
+    = NoPopup
+    | AdminPopup
+    | ManualPopup
+    | SignUpPopup
+    | NewNotebookPopup
+
+
+type alias FrontendModel =
+    { key : Browser.Navigation.Key
+    , url : Url.Url
+    , message : String
+    , messages : List Message
+    , appState : AppState
+    , appMode : AppMode
+    , currentTime : Time.Posix
+    , pressedKeys : List Keyboard.Key
+    , users : List Evergreen.V82.User.User
+    , stringData : Maybe String
+    , books : List Book
+    , currentBook : Book
+    , cellContent : String
+    , currentCellIndex : Int
+    , cloneReference : String
+    , deleteNotebookState : DeleteNotebookState
+    , showNotebooks : ShowNotebooks
+    , signupState : SignupState
+    , currentUser : Maybe Evergreen.V82.User.User
+    , inputUsername : String
+    , inputSignupUsername : String
+    , inputEmail : String
+    , inputRealname : String
+    , inputPassword : String
+    , inputPasswordAgain : String
+    , inputTitle : String
+    , windowWidth : Int
+    , windowHeight : Int
+    , popupState : PopupState
+    , showEditor : Bool
+    }
+
+
+type alias Username =
+    String
+
+
+type alias NoteBookDict =
+    Dict.Dict String Book
+
+
+type alias UserToNoteBookDict =
+    Dict.Dict Username NoteBookDict
+
+
+type alias NotebookRecord =
+    { id : String
+    , author : String
+    , public : Bool
+    }
+
+
+type alias BackendModel =
+    { message : String
+    , currentTime : Time.Posix
+    , randomSeed : Random.Seed
+    , uuidCount : Int
+    , uuid : String
+    , randomAtmosphericInt : Maybe Int
+    , userToNoteBookDict : UserToNoteBookDict
+    , slugDict : Dict.Dict String NotebookRecord
+    , authenticationDict : Evergreen.V82.Authentication.AuthenticationDict
+    }
+
+
+type FrontendMsg
+    = UrlClicked Browser.UrlRequest
+    | UrlChanged Url.Url
+    | NoOpFrontendMsg
+    | FETick Time.Posix
+    | KeyboardMsg Keyboard.Msg
+    | StringDataRequested Int String
+    | StringDataSelected Int String File.File
+    | StringDataLoaded Int String String
+    | NewCell Int
+    | DeleteCell Int
+    | EditCell Int
+    | ClearCell Int
+    | EvalCell Int
+    | InputElmCode Int String
+    | UpdateNotebookTitle
+    | NewNotebook
+    | ProposeDeletingNotebook
+    | CancelDeleteNotebook
+    | ChangeAppMode AppMode
+    | TogglePublic
+    | ClearNotebookValues
+    | SetCurrentNotebook Book
+    | CloneNotebook
+    | PullNotebook
+    | SetShowNotebooksState ShowNotebooks
+    | ChangePopup PopupState
+    | GotViewport Browser.Dom.Viewport
+    | GotNewWindowDimensions Int Int
+    | SignUp
+    | SignIn
+    | SignOut
+    | SetSignupState SignupState
+    | InputUsername String
+    | InputSignupUsername String
+    | InputPassword String
+    | InputPasswordAgain String
+    | InputEmail String
+    | InputTitle String
+    | InputCloneReference String
+    | AdminRunTask
+    | GetUsers
+
+
+type ToBackend
+    = NoOpToBackend
+    | RunTask
+    | SendUsers
+    | CreateNotebook String String
+    | SaveNotebook Book
+    | DeleteNotebook Book
+    | GetClonedNotebook String String
+    | GetPulledNotebook String String String String
+    | UpdateSlugDict Book
+    | GetUsersNotebooks String
+    | GetPublicNotebooks String
+    | SignUpBE String String String
+    | SignInBEDev
+    | SignInBE String String
+    | SignOutBE (Maybe String)
+    | UpdateUserWith Evergreen.V82.User.User
+
+
+type BackendMsg
+    = NoOpBackendMsg
+    | Tick Time.Posix
+
+
+type ToFrontend
+    = NoOpToFrontend
+    | MessageReceived Message
+    | GotUsers (List Evergreen.V82.User.User)
+    | GotNotebook Book
+    | GotNotebooks (List Book)
+    | SendMessage String
+    | UserSignedIn Evergreen.V82.User.User Lamdera.ClientId
+    | SendUser Evergreen.V82.User.User
